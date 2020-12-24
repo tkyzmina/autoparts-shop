bd9b50a72624a3e7d6c862bcf218cfab77f73b15
tkyzmina <tkyzmina@gmail.com>
2020-12-24 10:34:13 +0300

    добавляет переключение полей поиска

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
-'use strict';
-var pageHeader = document.querySelector('.header');
-var main = document.querySelector('main');
-var mobileMenu = document.querySelector('.header__mob-menu');
-var headerToggle = document.querySelector('.header__button');
+"use strict";
+var pageHeader = document.querySelector(".header");
+var main = document.querySelector("main");
+var mobileMenu = document.querySelector(".header__mob-menu");
+var headerToggle = document.querySelector(".header__button");
 
 // pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  headerToggle.classList.toggle('header__button--active');
-  mobileMenu.classList.toggle('header__mob-menu--show');
-  headerToggle.classList.toggle('header__button--mobile');
+headerToggle.addEventListener("click", function () {
+  headerToggle.classList.toggle("header__button--active");
+  mobileMenu.classList.toggle("header__mob-menu--show");
+  headerToggle.classList.toggle("header__button--mobile");
   // pageHeader.classList.toggle('header--show-menu');
   // main.classList.toggle('main--show-menu');
 
@@ -22,8 +22,7 @@ headerToggle.addEventListener('click', function () {
   // }
 });
 
-
-const slider = document.querySelector('.swiper-container');
+const slider = document.querySelector(".swiper-container");
 
 let mySwiper = new Swiper(slider, {
   slidesPerView: 1,
@@ -33,19 +32,19 @@ let mySwiper = new Swiper(slider, {
     loadPrevNext: true,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   scrollbar: {
-    hide: true
+    hide: true,
   },
-})
+});
 
-const sliderPop = document.querySelector('.swiper-container1');
+const sliderPop = document.querySelector(".swiper-container1");
 
 let mySwiper1 = new Swiper(sliderPop, {
   slidesPerView: 4,
@@ -59,43 +58,74 @@ let mySwiper1 = new Swiper(sliderPop, {
   },
 
   navigation: {
-    nextEl: '.swiper-next',
-    prevEl: '.swiper-prev',
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
   },
   breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 0
+      spaceBetween: 0,
     },
     767: {
       slidesPerView: 2,
-      spaceBetween: 10
+      spaceBetween: 10,
     },
     960: {
       slidesPerView: 3,
-      spaceBetween: 30
+      spaceBetween: 30,
     },
     1290: {
       slidesPerView: 4,
-      spaceBetween: 29
+      spaceBetween: 29,
     },
-  }
-})
-let triggers = document.querySelectorAll('.footer__list h3');
+  },
+});
+let triggers = document.querySelectorAll(".footer__list h3");
 
 triggers.forEach((item) =>
-  item.addEventListener('click', () => {
+  item.addEventListener("click", () => {
     const parent = item.nextElementSibling;
 
-    if (parent.classList.contains('js-submenu--active')) {
-      parent.classList.remove('js-submenu--active');
-      parent.classList.remove('hide');
+    if (parent.classList.contains("js-submenu--active")) {
+      parent.classList.remove("js-submenu--active");
+      parent.classList.remove("hide");
     } else {
-      document.querySelectorAll('.js-submenu').forEach((child) =>
-        child.classList.remove('js-submenu--active'))
-      parent.classList.add('js-submenu--active');
-      parent.classList.add('show');
+      document
+        .querySelectorAll(".js-submenu")
+        .forEach((child) => child.classList.remove("js-submenu--active"));
+      parent.classList.add("js-submenu--active");
+      parent.classList.add("show");
     }
   })
 );
+
+const searchControls = document.querySelectorAll(".search__item");
+const searchTabs = document.querySelectorAll(".search__form");
+const showSearchField = function () {
+  let dataNumer = "";
+  searchControls.forEach((el) => {
+    el.onclick = function (e) {
+      e.preventDefault();
+      selectConltol();
+      dataNumer = e.target.getAttribute("data-control");
+      this.classList.add("search__item--active");
+      showtab(dataNumer);
+    };
+  });
+  function selectConltol() {
+    searchControls.forEach((el) => {
+      el.classList.remove("search__item--active");
+    });
+  }
+  function showtab(number) {
+    searchTabs.forEach((el) => {
+      el.classList.remove("search__form--active");
+      let tabDataNumber = el.getAttribute("data-tab");
+      if (tabDataNumber === number) {
+        el.classList.add("search__form--active");
+      }
+    });
+  }
+};
+showSearchField();
